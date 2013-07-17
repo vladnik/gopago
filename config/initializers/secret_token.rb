5ba83d90f5667a62d7512447759dba105c597f8f
Volodymyr Ladnik <vladnik@allplaystudios.com>
2013-07-16 21:07:46 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gopago::Application.config.secret_key_base = 'aae22d568f2c6401a7df7262f0c02b15e7e14feb154490aae9f4e0ee0714e0f33c59748bbcf80d1170cac5c0273eb2197f4df91abd2d1dc86b85b2e8af469fcf'
